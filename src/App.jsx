a1b1b96c3ba84c340c991bdeb3f312a45b47fb20
ignatius22 <igee54077@gmail.com>
2023-10-14 07:31:03 +0100

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
 // import React from 'react'
-
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
 function App() {
-  return (
-    <div>Hello Vite</div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
