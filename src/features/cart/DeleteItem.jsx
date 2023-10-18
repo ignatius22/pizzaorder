ff7a5cb0c7b7a9c84d674ae8da1311896fa7c71b
ignatius22 <igee54077@gmail.com>
2023-10-18 15:37:19 +0100

    Add delete functionality

--- src/features/cart/DeleteItem.jsx
+++ src/features/cart/DeleteItem.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import Button from '../../ui/Button'
+import React from 'react';
+import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { deleteItem } from './cartSlice';
 
-function DeleteItem() {
+function DeleteItem({ pizzaId }) {
+  const dispatch = useDispatch();
   return (
-    <Button type="small">Delete</Button>
-  )
+    <Button type="small" onClick={() => dispatch(deleteItem(pizzaId))}>
+      Delete
+    </Button>
+  );
 }
 
-export default DeleteItem
+export default DeleteItem;
