24ee43ee49fd9bac1b7ccc6eeb27f26a858da897
ignatius22 <igee54077@gmail.com>
2023-10-18 16:58:38 +0100

    Improve menu item

--- src/features/cart/UpdateItem.jsx
+++ src/features/cart/UpdateItem.jsx
@@ -3,16 +3,17 @@ import Button from '../../ui/Button';
 import { useDispatch } from 'react-redux';
 import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 
-function UpdateItem({ pizzaId }) {
+function UpdateItem({ pizzaId, currentQuantity}) {
   const dispatch = useDispatch();
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-1 md:gap-3 items-center">
       <Button
         type="round"
         onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
       >
         -
       </Button>
+      <span className='text-sm font-medium'>{currentQuantity}</span>
       <Button
         type="round"
         onClick={() => dispatch(increaseItemQuantity(pizzaId))}
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
 import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 import DeleteItem from '../cart/DeleteItem';
+import UpdateItem from '../cart/UpdateItem';
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
@@ -43,7 +44,12 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItem pizzaId={id} currentQuantity={currentQuantity} />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
               Add to cart
