4dc54b6d40602d0581607a68c23f6ed9a8f42920
ignatius22 <igee54077@gmail.com>
2023-10-18 15:33:42 +0100

    Move button to deleteItem component

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,5 +1,6 @@
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import DeleteItem from './DeleteItem';
 
 function CartItem({ item }) {
   /* eslint-disable no-unused-vars */
@@ -13,7 +14,7 @@ function CartItem({ item }) {
       </p>
       <div className="flex items-center justify-between sm:gap-6 ">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <Button type="small">Delete</Button>
+        <DeleteItem />
       </div>
     </li>
   );
---
+++ src/features/cart/DeleteItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '../../ui/Button'
+
+function DeleteItem() {
+  return (
+    <Button type="small">Delete</Button>
+  )
+}
+
+export default DeleteItem
