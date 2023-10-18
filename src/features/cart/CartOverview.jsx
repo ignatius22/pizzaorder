5981e7a8bd0a8778522724ba0369473f791c7ec9
ignatius22 <igee54077@gmail.com>
2023-10-18 08:40:28 +0100

    Refactor uselector and implement add to cart feature

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getTotalCartPrice, getTotalCartQuantity } from './cartSlice';
+import { formatCurrency } from '../../utils/helpers';
 
 function CartOverview() {
+  const quantity = useSelector(getTotalCartQuantity);
+  const totalPrice = useSelector(getTotalCartPrice);
+
+  if(!quantity) return null;
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 uppercase text-stone-200 sm:px-6 md:text-base ">
-      <p className="space-x-4 sm:space-x-6 font-semibold text-stone-300">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
+        <span>{quantity} pizzas</span>
+        <span>{formatCurrency(totalPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
